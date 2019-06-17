c955f707780fc5015a26d5796fe5deed16cf0bbf
Ejaz Ahmed <ejazahmed@pop-os.localdomain>
2019-06-17 11:53:58 +0500

    Added sorting support for linkedlists

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([3000, 41, 54, 6, -1]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("XXaa");
+// const charactersCollection = new CharactersCollection("XXaa");
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(344);
+linkedList.add(-44);
+linkedList.add(44);
+linkedList.add(4);
+linkedList.add(2);
+
+const sorter = new Sorter(linkedList);
+
 sorter.sort();
-console.log(charactersCollection.data);
+
+linkedList.print();
