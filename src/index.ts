b1cad53fb5e3aff836e1b429c9d4c700f96a5c5b
Ejaz Ahmed <ejazahmed@pop-os.localdomain>
2019-06-17 10:19:43 +0500

    Initial commit for sorter algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-console.log("Hi there");
+class Sorter {
+  constructor(public collection: number[] | string) {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
 
-const logSomething = () => {
-  console.log("Something");
-};
+        if (this.collection === "string") {
+        }
+      }
+    }
+  }
+}
 
-logSomething();
+const sorter = new Sorter([3, 41, 54, 6, -1]);
+sorter.sort();
+
+console.log(sorter.collection);
