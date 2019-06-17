99b5437e7de9b6b20f03d4e8312003390967dd72
Ejaz Ahmed <ejazahmed@pop-os.localdomain>
2019-06-17 11:09:40 +0500

    Added support for string

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([3000, 41, 54, 6, -1]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([3000, 41, 54, 6, -1]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("XXaa");
+
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
