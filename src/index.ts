2bf84ffb480ca436a3ccd63e7289ebcfd98a0704
Ejaz Ahmed <ejazahmed@pop-os.localdomain>
2019-06-17 10:51:25 +0500

    V1 of sorter algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-        if (this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([3, 41, 54, 6, -1]);
+const numbersCollection = new NumbersCollection([3000, 41, 54, 6, -1]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 
-console.log(sorter.collection);
+console.log(numbersCollection.data);
